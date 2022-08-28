de26f590828a3230e3afb648a5ac6b44600e8cc0
SlowingDisc <99830088+SlowingDisc@users.noreply.github.com>
2022-08-28 15:32:24 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -17,28 +17,28 @@ local Section2 = Tab:CreateSection('Normal')
 
 local Button = Section2:CreateButton('Mad City', function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/New-Update/main/Update%20New.lua", true))()
-	game.CoreGui.[Visual UI Library | .gg/puxxCphTnK]:Destroy()
+
 end)
 
 local Button2 = Section2:CreateButton('Crazy Pet Simulator', function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/crazy-pet-obfuse/main/crazy%20pet%20obfuse.lua", true))()
-	game.CoreGui.[Visual UI Library | .gg/puxxCphTnK]:Destroy()
+
 end)
 
 local Button3 = Section2:CreateButton('Batting Champions', function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/batting-Champions/main/Champions.lua", true))()
-	game.CoreGui.[Visual UI Library | .gg/puxxCphTnK]:Destroy()
+
 end)
 
 local Button4 = Section2:CreateButton('Fight in Train station', function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/fight/main/fight.lua", true))()
-	game.CoreGui.[Visual UI Library | .gg/puxxCphTnK]:Destroy()
+
 end)
 
 
 local Button5 = Section2:CreateButton('Drinking Simulator', function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/Drinking-Simulator/main/Drinking%20Simulator.lua", true))()
-	game.CoreGui.[Visual UI Library | .gg/puxxCphTnK]:Destroy()
+
 end)
 
 
